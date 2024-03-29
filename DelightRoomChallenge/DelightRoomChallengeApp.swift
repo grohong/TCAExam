8e36a36a6468aae63b4af2cf5b406b8cc2309518
grohong <grohong76@gmail.com>
2024-03-29 17:38:25 +0900

    프로젝트 설정

---
+++ DelightRoomChallenge/DelightRoomChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelightRoomChallengeApp.swift
+//  DelightRoomChallenge
+//
+//  Created by Hong Seong Ho on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct DelightRoomChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
