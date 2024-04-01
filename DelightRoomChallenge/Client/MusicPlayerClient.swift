95d88ed46edd14a72ff0f2de268c01f53a2dcc40
grohong <grohong76@gmail.com>
2024-04-02 00:17:07 +0900

    MusicPlayerClient 설정

---
+++ DelightRoomChallenge/Client/MusicPlayerClient.swift
@@ -0,0 +1,53 @@
+//
+//  MusicPlayerClient.swift
+//  DelightRoomChallenge
+//
+//  Created by Hong Seong Ho on 4/2/24.
+//
+
+import Foundation
+import Dependencies
+import Entities
+import MusicPlayer
+
+struct MusicPlayerClient {
+
+    var play: @Sendable () async -> Void
+    var pause: @Sendable () async -> Void
+    var startAlbum: @Sendable (([Music], Int)) async -> Void
+    var nextPlay: @Sendable () async -> Void
+    var prevPlay: @Sendable () async -> Void
+    var currentMusic: @Sendable () -> AsyncStream<Music?>
+    var period: @Sendable () -> AsyncStream<Double>
+}
+
+extension MusicPlayerClient: DependencyKey {
+
+    static let liveValue = Self(
+        play: { await MusicPlayerManager.shared.play() },
+        pause: { await MusicPlayerManager.shared.pause() },
+        startAlbum: { musicList, index in await MusicPlayerManager.shared.startPlay(musicList: musicList, index: index) },
+        nextPlay: { await MusicPlayerManager.shared.nextPlay() },
+        prevPlay: { await MusicPlayerManager.shared.prevPlay() },
+        currentMusic: {
+            AsyncStream { continuation in
+                let musicPlayerManager = MusicPlayerManager.shared
+                Task { await musicPlayerManager.configureCurrentMusicContinuation(continuation) }
+            }
+        },
+        period: {
+            AsyncStream { continuation in
+                let musicPlayerManager = MusicPlayerManager.shared
+                Task { await musicPlayerManager.configurePeriodContinuation(continuation) }
+            }
+        }
+    )
+}
+
+extension DependencyValues {
+
+    var musicPlayerClient: MusicPlayerClient {
+        get { self[MusicPlayerClient.self] }
+        set { self[MusicPlayerClient.self] = newValue }
+    }
+}
