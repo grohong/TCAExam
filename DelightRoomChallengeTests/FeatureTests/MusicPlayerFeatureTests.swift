6d0fa5d41f49ad1d43e8dbbfa71abefaa2e59130
grohong <grohong76@gmail.com>
2024-04-02 03:50:36 +0900

    MusicPlayerFeature 테스트 추가

---
+++ DelightRoomChallengeTests/FeatureTests/MusicPlayerFeatureTests.swift
@@ -0,0 +1,97 @@
+//
+//  MusicPlayerFeatureTests.swift
+//  DelightRoomChallengeTests
+//
+//  Created by Hong Seong Ho on 4/2/24.
+//
+
+import XCTest
+@testable import DelightRoomChallenge
+import ComposableArchitecture
+import Entities
+
+final class MusicPlayerFeatureTests: XCTestCase {
+
+    let mockMusicPlayerClient = MusicPlayerClient(
+        play: { },
+        pause: { },
+        startAlbum: { musicList, index in },
+        nextPlay: { },
+        prevPlay: { },
+        currentMusic: { AsyncStream { continuation in } },
+        period: { AsyncStream { continuation in } }
+    )
+
+    @MainActor
+    func testPause() async {
+        let store = TestStore(initialState: MusicPlayerFeature.State()) {
+            MusicPlayerFeature()
+        } withDependencies: {
+            $0.musicPlayerClient = mockMusicPlayerClient
+        }
+
+        await store.send(.pause)
+        await store.receive(.isPlayingChanged(false)) {
+            $0.isPlaying = false
+        }
+    }
+
+    @MainActor
+    func testPlay() async {
+        let store = TestStore(initialState: MusicPlayerFeature.State(isPlaying: false)) {
+            MusicPlayerFeature()
+        } withDependencies: {
+            $0.musicPlayerClient = mockMusicPlayerClient
+        }
+
+        await store.send(.play)
+        await store.receive(.isPlayingChanged(true)) {
+            $0.isPlaying = true
+        }
+    }
+
+    @MainActor
+    func testSycnPeriod() async {
+        let periodContinuation = PeriodContinuationManager()
+
+        let mockMusicPlayerClient = MusicPlayerClient(
+            play: { },
+            pause: { },
+            startAlbum: { musicList, index in },
+            nextPlay: { },
+            prevPlay: { },
+            currentMusic: { AsyncStream { continuation in } },
+            period: {
+                AsyncStream { continuation in
+                    Task { await periodContinuation.configure(continuation) }
+                }
+            }
+        )
+
+        let store = TestStore(initialState: MusicPlayerFeature.State()) {
+            MusicPlayerFeature()
+        } withDependencies: {
+            $0.musicPlayerClient = mockMusicPlayerClient
+        }
+
+        store.exhaustivity = .off(showSkippedAssertions: false)
+        await store.send(.onTask)
+        await periodContinuation.yield(period: 0.5)
+        await store.receive(.periodChanged(0.5)) {
+            $0.period = 0.5
+        }
+    }
+}
+
+actor PeriodContinuationManager {
+
+    var continuation: AsyncStream<Double>.Continuation?
+
+    func yield(period: Double) {
+        continuation?.yield(period)
+    }
+
+    func configure(_ continuation: AsyncStream<Double>.Continuation) {
+        self.continuation = continuation
+    }
+}
