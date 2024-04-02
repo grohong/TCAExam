8359d2fa1483764c3784de02ee4c96dd1ef0e541
grohong <grohong76@gmail.com>
2024-04-02 16:51:42 +0900

    background 음악 설정

--- MusicPlayer/Sources/MusicPlayer/MusicPlayerManager.swift
+++ MusicPlayer/Sources/MusicPlayer/MusicPlayerManager.swift
@@ -14,13 +14,20 @@ public actor MusicPlayerManager {
 
     private(set) var musicList = [Music]()
     private(set) var currentPlayIndex: Int = .zero
-    private let player :PlayerProtocol
+    private let player: PlayerProtocol
     private var currentMusicContinuation: AsyncStream<Music?>.Continuation?
     private var periodContinuation: AsyncStream<Double>.Continuation?
     private var timeObserverToken: Any?
 
     init(player: PlayerProtocol = AVPlayer()) {
         self.player = player
+        configureRemoteCommandCenter()
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Failed to set up audio session: \(error)")
+        }
     }
 
     public func configureCurrentMusicContinuation(_ continuation: AsyncStream<Music?>.Continuation) async {
@@ -65,8 +72,17 @@ public actor MusicPlayerManager {
     }
 
     private func playCurrentIndexMusic() async {
-        let item = AVPlayerItem(url: musicList[currentPlayIndex].assetURL)
+        let music = musicList[currentPlayIndex]
+        let item = AVPlayerItem(url: music.assetURL)
         player.changeCurrentItem(with: item)
+        currentMusicContinuation?.yield(musicList[currentPlayIndex])
+        await play()
+        configurePlayerItemDidReachEndNotification(item)
+        startTrackingPeriod()
+        await configureMPNowPlayingInfoCenter(item, music: music)
+    }
+
+    private func configurePlayerItemDidReachEndNotification(_ item: AVPlayerItem) {
         NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: nil)
         NotificationCenter.default.addObserver(
             self,
@@ -74,9 +90,6 @@ public actor MusicPlayerManager {
             name: .AVPlayerItemDidPlayToEndTime,
             object: item
         )
-        startTrackingPeriod()
-        currentMusicContinuation?.yield(musicList[currentPlayIndex])
-        await play()
     }
 
     @MainActor
@@ -87,10 +100,16 @@ public actor MusicPlayerManager {
     private func startTrackingPeriod() {
         let interval = CMTime(seconds: 1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
         timeObserverToken = player.addPeriodTimeObserver(forInterval: interval, queue: DispatchQueue.main) { [weak self] time in
-            guard let self = self, let duration = self.player.currentItem?.duration else { return }
-            let durationInSeconds = CMTimeGetSeconds(duration)
+            guard let self = self, let playerItem = self.player.currentItem else { return }
+            let currentTimeInSeconds = CMTimeGetSeconds(time)
+            let durationInSeconds = CMTimeGetSeconds(playerItem.duration)
+
+            var nowPlayingInfo = MPNowPlayingInfoCenter.default().nowPlayingInfo ?? [String: Any]()
+            nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = currentTimeInSeconds
+            nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = durationInSeconds
+            MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+
             if durationInSeconds.isFinite {
-                let currentTimeInSeconds = CMTimeGetSeconds(time)
                 let progress = currentTimeInSeconds / durationInSeconds
                 Task { await self.updatePeriod(progress) }
             }
@@ -108,6 +127,7 @@ protocol PlayerProtocol {
     func changeCurrentItem(with item: AVPlayerItem)
     func addPeriodTimeObserver(forInterval interval: CMTime, queue: DispatchQueue, using: @escaping (CMTime) -> Void)
     var currentItem: AVPlayerItem? { get }
+    var rate: Float { get }
 }
 
 extension AVPlayer: PlayerProtocol {
@@ -118,5 +138,66 @@ extension AVPlayer: PlayerProtocol {
 
     func addPeriodTimeObserver(forInterval interval: CMTime, queue: DispatchQueue, using: @escaping (CMTime) -> Void) {
         addPeriodicTimeObserver(forInterval: interval, queue: queue, using: using)
+    }
+}
+
+import MediaPlayer
+
+extension MusicPlayerManager {
+
+    private func configureMPNowPlayingInfoCenter(_ item: AVPlayerItem, music: Music) async {
+        var nowPlayingInfo = [String: Any]()
+        nowPlayingInfo[MPMediaItemPropertyTitle] = music.title
+        nowPlayingInfo[MPMediaItemPropertyArtist] = music.artist
+        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = item.duration.seconds.isFinite
+        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = item.currentTime().seconds
+        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = player.rate
+
+        if let artworkImage = await item.asset.thumbnail {
+            let artwork = MPMediaItemArtwork(boundsSize: artworkImage.size) { size in return artworkImage }
+            nowPlayingInfo[MPMediaItemPropertyArtwork] = artwork
+        }
+
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+    }
+
+    private func configureRemoteCommandCenter() {
+        let commandCenter = MPRemoteCommandCenter.shared()
+        commandCenter.playCommand.addTarget { [weak self] event in
+            Task { await self?.play() }
+            return .success
+        }
+        commandCenter.pauseCommand.addTarget { [weak self] event in
+            Task { await self?.pause() }
+            return .success
+        }
+    }
+}
+
+extension AVAsset {
+
+    public var thumbnail: UIImage? {
+        get async {
+            guard let metadata = try? await self.loadCommonMetadata(),
+                  let artworkMetadata = metadata.first(where: { $0.commonKey == .commonKeyArtwork }),
+                  let imageData = try? await artworkMetadata.load(.dataValue) else { return nil }
+            return UIImage(data: imageData)
+        }
+    }
+
+    private func loadCommonMetadata() async throws -> [AVMetadataItem] {
+        try await withCheckedThrowingContinuation { continuation in
+            self.loadMetadata(for: .id3Metadata) { metadata, error in
+                if let metadata {
+                    continuation.resume(returning: metadata)
+                    return
+                }
+
+                if let error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+            }
+        }
     }
 }
