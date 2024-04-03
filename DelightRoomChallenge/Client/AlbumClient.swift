6bf606439560a73c2d932ae46d119db2cbf36911
grohong <grohong76@gmail.com>
2024-04-03 21:08:44 +0900

    liveValue에서 앱내 라이브러리에서 노래를 가지고 오도록 설정

--- DelightRoomChallenge/Client/AlbumClient.swift
+++ DelightRoomChallenge/Client/AlbumClient.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import MediaPlayer
 import Dependencies
 import Entities
 
@@ -17,6 +18,40 @@ struct AlbumClient {
 extension AlbumClient: DependencyKey {
 
     static let liveValue = Self(
+        fetchAlbumList: {
+            let status = await withUnsafeContinuation { continuation in
+                MPMediaLibrary.requestAuthorization { status in
+                    continuation.resume(with: .success(status))
+                }
+            }
+
+            guard status == .authorized else { return Album.mockAlbumList }
+
+            let predicate = MPMediaPropertyPredicate(value: false, forProperty: MPMediaItemPropertyIsCloudItem)
+            let query = MPMediaQuery.songs()
+            query.addFilterPredicate(predicate)
+            guard let mediaItemList = query.items else { return [] }
+
+            let albumsDictionary = Dictionary(grouping: mediaItemList) { $0.albumTitle ?? "Unknown Album" }
+
+            return albumsDictionary.compactMap { albumTitle, items in
+                guard let firstItem = items.first, let artist = firstItem.albumArtist else { return nil }
+
+                let musicList: [Music] = items.compactMap { item in
+                    guard let title = item.title, let assetURL = item.assetURL else { return nil }
+                    return Music(id: UUID(), title: title, artist: artist, assetURL: assetURL)
+                }
+
+                return musicList.isEmpty ? nil : Album(id: UUID(), title: albumTitle, artist: artist, musicList: musicList)
+            }
+        }
+    )
+
+    static let previewValue = Self(
+        fetchAlbumList: { Album.mockAlbumList }
+    )
+
+    static let testValue = Self(
         fetchAlbumList: { Album.mockAlbumList }
     )
 }
